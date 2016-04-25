6b75170a3313ee29a056ca626a719e768ceea1cd
EGOIST <0x142857@gmail.com>
2016-04-25 22:11:50 +0800

    init

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict'
+const koa = require('koa')
+const Router = require('koa-router')
+const im = require('imagemagick')
+const request = require('request')
+const sharp = require('sharp')
+
+const app = koa()
+const router = new Router()
+
+function getImage(cb) {
+  const url = this.query.url
+  if (!url) {
+    return cb(new Error('no image url provided'))
+  }
+
+  let contentType = ''
+  let imageType = 'jpeg'
+
+  const width = this.query.width && parseInt(this.query.width)
+  const height = this.query.height && parseInt(this.query.height)
+  const transform = sharp()
+    .resize(width, height)
+    [imageType]()
+    .toBuffer((err, buf, info) => {
+      if (err) {
+        return cb(err)
+      }
+      cb(null, {buf, contentType})
+    })
+
+  request(url)
+    .on('response', function(response) {
+      contentType = response.headers['content-type']
+      imageType = contentType.substr(contentType.indexOf('/') + 1)
+    })
+    .pipe(transform)
+}
+
+router.get('/', function* () {
+  try {
+    const ret = yield getImage
+    this.type = ret.contentType
+    this.body = ret.buf
+  } catch (e) {
+    this.body = {
+      error: e.message
+    }
+  }
+})
+
+app.use(router.routes())
+
+const port = process.env.NODE_PORT || 4000
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
